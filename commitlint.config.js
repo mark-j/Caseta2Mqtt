4f38e67f1dbaf0e0c43a5e0a9482fab96add689d
Mark Johnson <mark@markjohnson.io>
2020-08-01 23:28:19 -0400

    Updated config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-atom',
+  formatter: '@commitlint/format',
   rules: {
     'type-enum': [0, 'always', ['feat', 'fix', 'chore', 'ci']],
   }
