ba4ac794ffd24ff5cacc2f53469c0642064153fc
Mark Johnson <mark@markjohnson.io>
2020-08-02 01:13:19 -0400

    Added docs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'refactor']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'refactor', 'docs']],
   }
 };
