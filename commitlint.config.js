ac6a1f7a449f4490afe7a32ae78eec8cb42a897a
Mark Johnson <mark@markjohnson.io>
2020-08-01 23:45:31 -0400

    Added test workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  formatter: '@commitlint/format',
   rules: {
     'type-enum': [0, 'always', ['feat', 'fix', 'chore', 'ci']],
   }
