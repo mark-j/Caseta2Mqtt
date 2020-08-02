71a748304d69baf448f538a1f0443b3cfc38b71e
Mark Johnson <mark@markjohnson.io>
2020-08-02 00:27:57 -0400

    chore: clean up CI and readme (#5)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
-  formatter: '@commitlint/format'
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'ci']],
+  }
 };
